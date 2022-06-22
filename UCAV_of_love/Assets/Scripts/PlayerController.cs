c4f18397db9f9f3d0eeffbc672e92a1c75450e6f
BogdanDevichev <bogdandevichev@gmail.com>
2022-06-22 22:36:26 +0300

    Player can move.

---
+++ UCAV_of_love/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        playerRb.AddForce(Vector3.forward * speed * verticalInput * Time.deltaTime);
+    }
+}
